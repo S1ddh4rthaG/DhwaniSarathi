3d72ac24863953c9ad8a812932ff58fff7dbb7c2
sirish <sirish1527@gmail.com>
2023-12-14 00:13:25 +0530

    Frontend -Auth userType context set

--- audiometer/FirebaseConfig.js
+++ audiometer/FirebaseConfig.js
@@ -3,8 +3,6 @@ import { initializeApp } from "firebase/app";
 import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { getFirestore } from "firebase/firestore";
-import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
