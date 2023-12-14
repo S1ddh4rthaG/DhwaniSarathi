b4a8aecea95a5c6d1ffd4a4424799950e53e38ca
sirish <sirish1527@gmail.com>
2023-12-15 00:48:05 +0530

    expo layout

---
+++ audiometer/app/_layout.js
@@ -0,0 +1,11 @@
+import { Stack } from "expo-router";
+
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{ headerStyle: { backgroundColor: "orange" } }}
+    >
+   
+    </Stack>
+  );
+}
