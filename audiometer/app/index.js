85bccb72eb2e872755fffe4fc59da64996fbd48f
Swami Kedari <swamikedarics48@gmail.com>
2023-12-19 15:15:27 +0530

    Added cards in the home page

--- audiometer/app/index.js
+++ audiometer/app/index.js
@@ -17,7 +17,7 @@ export default () => {
     return <Redirect href="/Screens/Home" />;
   }
 
-  return <Redirect href="/Screens/Login" />;
+  return <Redirect href="/Screens/HomeNew" />;
 };
 
 // const styles = StyleSheet.create({
