2de05e22bbbccdb2a495965c4178615c8602a134
SiddharthaG <91111745+S1ddh4rthaG@users.noreply.github.com>
2023-12-19 15:00:52 +0530

    Fixed index

--- audiometer/app/index.js
+++ audiometer/app/index.js
@@ -17,7 +17,7 @@ export default () => {
     return <Redirect href="/Screens/Home" />;
   }
 
-  return <Redirect href="/AudiometryTest" />;
+  return <Redirect href="/Screens/Login" />;
 };
 
 // const styles = StyleSheet.create({
