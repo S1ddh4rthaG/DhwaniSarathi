bd3260581fe807d3d9e040cfa5d0be987b109afb
Preethi Varsha <82671452+PreethiVarshaM@users.noreply.github.com>
2023-12-19 13:17:17 +0530

    Frontend: Educator theme fix 1

--- audiometer/app/Screens/Educator/EducatorHome.js
+++ audiometer/app/Screens/Educator/EducatorHome.js
@@ -8,18 +8,21 @@ import { baseurl } from '../../Constants/ip.js';
 
 
 export default function EducatorHome() {
-    
+
     const params = useLocalSearchParams();
     const [profile, setProfile] = useState({});
     const [loading, setLoading] = useState(true);
+    const [currentDate, setCurrentDate] = useState('');
+    const [classroomName, onChangeName] = React.useState('Enter Classroom Name');
+
 
     useEffect(() => {
         //get the profile data from the backend
         const fetchData = async () => {
             var EID;
-            if (params.EID){
+            if (params.EID) {
 
-               EID = params.EID;
+                EID = params.EID;
             }
             const url = `${baseurl}/educators/${EID}`;
 
@@ -39,49 +42,65 @@ export default function EducatorHome() {
         };
         fetchData();
 
+        // Function to get the current date
+        const fetchCurrentDate = () => {
+            const date = new Date();
+            const year = date.getFullYear();
+            const month = date.getMonth() + 1; // Month is zero-indexed
+            const day = date.getDate();
+
+            const formattedDate = `${day < 10 ? '0' + day : day}-${month < 10 ? '0' + month : month}-${year}`;
+            setCurrentDate(formattedDate);
+        };
+
+        fetchCurrentDate(); // Fetch the date when the component mounts
+
     }, []);
 
-   
+
     //  const sampleProfile =
     //     {
     //         "EID": "124",
     //         "EducatorName": "Sachin",
     //         "InstituteName": "IIT Tirupati"
     //     }
-     
+
 
     return (
-        (loading == true) ? (<Text>Loading...</Text> ):
-        
-        (<KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={{ flex: 1 }}
-        >
-            <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                <ScrollView style={styles.container2}>
-                    <View style={styles.header}>
+        (loading == true) ? (<Text>Loading...</Text>) :
 
-                        <ImageBackground source={require('../../assets/background.png')}
-                            style={styles.backgroundImage} />
-                        <View style={[styles.content, { backgroundColor: 'white', opacity: 0.6 }]}>
-                            <Text style={[styles.text, styles.heading]}>Educator Profile Page</Text>
-                        </View>
-
-                        <View style={styles.headerContent}>
-                            <View style={{ flex: 1 }}>
-                                <Text style={styles.name}>Welcome</Text>
-                                <Text style={styles.userInfo}>{profile.EducatorName}</Text>
-                                <Text style={styles.userInfo}>{profile.InstituteName}</Text>
+            (<KeyboardAvoidingView
+                behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                style={{ flex: 1 }}
+            >
+                <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+                    <ScrollView style={styles.container2}>
+                        <View style={styles.header}>
+                            <View style={[styles.content, { backgroundColor: '#eb455f', opacity: 1 }]}>
+                                <Text style={[styles.text, styles.heading, { color: 'white' }]}>Educator Profile Page</Text>
                             </View>
-                            <View>
-                                <Image
-                                    style={styles.avatar}
-                                    source={require('../../assets/profile.png')}
-                                />
-                            </View>
-                        </View>
+                            <ImageBackground source={require('../../assets/background.png')}
+                                style={styles.backgroundImage} >
 
-                        {/* <View style={styles.content}>
+
+                                <View style={styles.headerContent}>
+                                    <View>
+                                        <Image
+                                            style={styles.avatar}
+                                            source={require('../../assets/profile.png')}
+                                        />
+                                    </View>
+                                    <View style={{ flex: 1, flexWrap: 'wrap-reverse' }}>
+                                        <Text style={styles.name}>Welcome</Text>
+                                        <Text style={styles.userInfo}>{profile.EducatorName}</Text>
+                                        <Text style={styles.userInfo}>{profile.InstituteName}</Text>
+                                    </View>
+
+                                </View>
+                            </ImageBackground>
+
+
+                            {/* <View style={styles.content}>
 
                             <View style={styles.profileDataContainer}>
                                 <Text style={styles.ptext}>{profile.Age} years</Text>
@@ -92,54 +111,72 @@ export default function EducatorHome() {
                         </View> */}
 
 
-                    </View>
-
-                    <View style={styles.body}>
-
-                        <Pressable style={styles.RectangleShapeView}>
-                            <Text style={styles.headtText}>Date</Text>
-                            <Text style={styles.SubjectText}>30 Sept, 2022 </Text>
-                        </Pressable>
-                        <View styles={{
-                            flexDirection: 'row', alignItems: 'center', margin: 0, padding: 0
-                        }}>
-
-
-                            {/* create a new classroom on button click here*/}
-
-                            <TouchableOpacity style={styles.btn} onPress={() => Alert.alert('Button pressed')}>
-                                <Text style={styles.btnText}>Create Classroom</Text>
-                            </TouchableOpacity>
-
-
-
-                        </View>
-                        <View style={{ width: '100%', backgroundColor: '#FFD700', height: 40, justifyContent: 'center' }}>
-                            <Text style={{ textAlign: 'center', color: 'black', fontWeight: 'bold', fontSize: 20 }}>Classrooms</Text>
                         </View>
 
-                        <ClassroomList EID = {profile.EID} />
+                        <View style={styles.body}>
 
-                    </View>
+                            <Pressable style={styles.RectangleShapeView}>
+                                <Text style={styles.headtText}>Date: {currentDate}</Text>
+                            </Pressable>
+                            <View styles={{
+                                flexDirection: 'row', alignItems: 'center', margin: 0, padding: 0
+                            }}>
 
-                </ScrollView>
-            </TouchableWithoutFeedback>
-        </KeyboardAvoidingView>)
+                                <View style={{
+                                    width: '100%',  // To align the avatar and texts horizontally,
+                                    margin: 10,
+                                    padding: 0,
+                                    flexDirection: 'row',
+
+                                }}>
+                                    {/* create a new classroom on button click here*/}
+                                    <TextInput
+                                        style={styles.input}
+                                        onChangeText={onChangeName}
+                                        value={classroomName}
+                                    />
+                                    <TouchableOpacity style={styles.btn} onPress={() => Alert.alert('Button pressed')}>
+                                        <Text style={styles.btnText}>Create Classroom</Text>
+                                    </TouchableOpacity>
+
+                                </View>
+
+                            </View>
+                            <View style={{ width: '100%', backgroundColor: '#2b3467', height: 50, justifyContent: 'center' }}>
+                                <Text style={{ textAlign: 'center', color: 'white', fontWeight: 'bold', fontSize: 20 }}>Classrooms</Text>
+                            </View>
+
+                            <ClassroomList EID={profile.EID} />
+
+                        </View>
+
+                    </ScrollView>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>)
     );
-                    
+
 }
 
 const styles = StyleSheet.create({
     header: {
-        height: 300
+        height: 250,
+        backgroundColor: '#2b3467',
+        flex: 1,
+        justifyContent: 'center',
+
     },
     headerContent: {
         padding: 30,
         alignItems: 'center',
-        display: 'flex',
+        display: 'flex', // To align the avatar and texts horizontally,
         flex: 1,
         flexDirection: 'row',
-        flexWrap: 'wrap'
+        flexWrap: 'wrap',
+        justifyContent: 'space-evenly',
+        marginRight: 30,
+        marginTop: 20,
+
+
     },
     avatar: {
         width: 100,
@@ -148,6 +185,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: 'white',
         marginBottom: 10,
+        marginLeft: 30,
     },
     location: {
         borderColor: 'white',
@@ -164,14 +202,15 @@ const styles = StyleSheet.create({
     },
     name: {
         fontSize: 22,
-        color: 'black',
+        color: '#bad7e9',
         fontWeight: 'bold',
     },
     headtText: {
         color: 'grey',
         fontWeight: 'bold',
         marginLeft: 20,
-        marginTop: 10
+        marginTop: 10,
+        alignSelf: 'center',
     },
     SubjectText: {
         color: 'black',
@@ -186,20 +225,21 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
     btn: {
-        backgroundColor: 'black',
+        backgroundColor: '#eb455f',
         borderRadius: 10,
-        width: '80%',
-        margin: 10,
-        borderColor: 'black', borderWidth: 1, borderRadius: 10, padding: 10, marginTop: 20,
-        height: 80,
+        width: '40%',
+
+        borderColor: '#bad7e9', borderWidth: 1, borderRadius: 10, padding: 10, marginTop: 20,
+
         textAlign: 'center',
         justifyContent: 'center',
         alignContent: 'center',
     },
     btnText: {
         color: 'white',
-        fontSize: 24,
+        fontSize: 14,
         fontWeight: 'bold',
+        textAlign: 'center',
     },
     body: {
         backgroundColor: 'white',
@@ -209,8 +249,8 @@ const styles = StyleSheet.create({
 
     RectangleShapeView: {
         marginTop: 20,
-        width: '80%',
-        height: 80,
+        width: '60%',
+        height: 40,
         backgroundColor: 'white',
         color: 'black',
         borderRadius: 10,
@@ -220,10 +260,9 @@ const styles = StyleSheet.create({
     },
     backgroundImage: {
         flex: 1,
-        height: 300,
+        height: 200,
         width: 100 + '%',
-        resizeMode: 'cover', // You can adjust the resizeMode as needed
-        // Other background styles if required
+        resizeMode: 'cover',
     },
     content: {
         justifyContent: 'center',
@@ -255,4 +294,16 @@ const styles = StyleSheet.create({
     container2: {
         flex: 1,
     },
+    input: {
+        height: 40,
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+        borderRadius: 10,
+        borderColor: 'grey',
+        color: 'grey',
+        textAlign: 'center',
+
+    },
 });
+
